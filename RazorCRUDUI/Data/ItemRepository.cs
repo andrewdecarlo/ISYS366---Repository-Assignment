de0e21c64c5c1507bcc37dd0785c17b69871976a
andrewdecarlo <34290135+andrewdecarlo@users.noreply.github.com>
2024-03-27 12:08:02 -0400

    Add project files.

---
+++ RazorCRUDUI/Data/ItemRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.SignalR.Protocol;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using RazorCRUDUI.Data;
+using RazorCRUDUI.Migrations;
+using RazorCRUDUI.Models;
+
+namespace RazorRepoUI.Data
+{
+    public class ItemRepository: IItemRepository
+    {
+        private readonly ItemsContext _context;
+        public ItemRepository(ItemsContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ItemModel>> GetItemsAsync(String? SearchString)
+        {
+            //select all items from database
+            var items = from i in _context.Items select i;
+
+            //if search string is not null or empty, filter by search string
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                items = items.Where(s => s.Name.Contains(SearchString));
+            }
+
+            return await items.ToListAsync();
+        }
+
+        public async Task<ItemModel?> GetItemByIdAsync(int id)
+        {
+            return await _context.Items.FirstOrDefaultAsync(i => i.ID == id);
+        }
+
+        public async Task<bool> UpdateItemAsync(ItemModel item)
+        {
+            _context.Attach(item).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public async Task<bool> DeleteItemAsync(int id)
+        {
+            var itemmodel = await _context.Items.FindAsync(id);
+
+            if (itemmodel != null)
+            {
+                _context.Items.Remove(itemmodel);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<ItemModel> InsertItemAsync(ItemModel item)
+        {
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+            return item;
+        }
+    }
+}
---
+++ RazorCRUDUI/Pages/Items/Details.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorCRUDUI.Data;
+using RazorCRUDUI.Models;
+
+namespace RazorCRUDUI.Pages.Items
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorCRUDUI.Data.ItemsContext _context;
+
+        public DetailsModel(RazorCRUDUI.Data.ItemsContext context)
+        {
+            _context = context;
+        }
+
+        public ItemModel ItemModel { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var itemmodel = await _context.Items.FirstOrDefaultAsync(m => m.ID == id);
+            if (itemmodel == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                ItemModel = itemmodel;
+            }
+            return Page();
+        }
+
+
+        // SYNCHRONOUS ONGET METHOD
+        //public IActionResult OnGet(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var itemmodel = _context.Items.FirstOrDefault(m => m.ID == id);
+        //    if (itemmodel == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    else
+        //    {
+        //        ItemModel = itemmodel;
+        //    }
+        //    return Page();
+        //}
+    }
+}
